e0015b7a855b4429207ea48b1c9dca9eb41ed4b8
AratiThorat <aratithorat@gmil.com>
2021-12-20 18:03:00 +0530

    first commit

---
+++ src/test/java/qapractice/mavenselenium/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qapractice.mavenselenium;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
---
+++ src/test/java/qapractice/mavenselenium/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qapractice.mavenselenium;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserTest()
+	{
+		System.out.println("BrowserTest");
+	}
+	
+	@Test
+	public void URLTest()
+	{
+		System.out.println("URLTest");
+	}
+	
+	
+
+}
